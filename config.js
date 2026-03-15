f9976ea62af75165d0a086732526e96d0d087f6e
Tihomir junior Lulevic <tihche@Tihomirs-MacBook-Air.local>
2026-03-15 14:55:48 +0100

    Add config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDsehCOqGHjvbex1zS2UTyV5DoqzZuCPP8",
+  authDomain: "skincard-12862.firebaseapp.com",
+  projectId: "skincard-12862",
+  storageBucket: "skincard-12862.firebasestorage.app",
+  messagingSenderId: "297337529431",
+  appId: "1:297337529431:web:cb3b1459ee104666736f12",
+  measurementId: "G-H8N7JSFVDB"
+};
+
+export const OPENAI_CONFIG = {
+  apiKey: ""
+};
+
+export const CARD_CONFIG = {
+  holderName: "YOUR NAME",
+  expiry: "12/28",
+  last4: "1234",
+  bankName: "SkinCard"
+};
